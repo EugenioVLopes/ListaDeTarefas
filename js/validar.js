be9cd714eb07ed57db563730e10ed7011da14e14
Eugenio-Lopes <128297188+Eugenio-Lopes@users.noreply.github.com>
2023-06-28 11:30:40 -0300

    login page add

---
+++ js/validar.js
@@ -0,0 +1,34 @@
+const usuarios = [
+  {
+    login: "eugenio",
+    pass: "eugenio",
+  },
+  {
+    login: "lucas",
+    pass: "lucas",
+  },
+  {
+    login: "admin",
+    pass: "admin",
+  },
+];
+
+let botao = document.getElementById("btn-login");
+
+botao.addEventListener("click", function logar() {
+  let reqUsuario = document.getElementById("usuario").value;
+  let reqSenha = document.getElementById("senha").value;
+  let validaLogin = false;
+
+  for (let i in usuarios) {
+    if (reqUsuario === usuarios[i].login && reqSenha === usuarios[i].pass) {
+      validaLogin = true;
+      break;
+    }
+  }
+  if (validaLogin == true) {
+    location.href="index.html";
+  } else {
+    alert("Usuário ou senha inválido");
+  }
+});
